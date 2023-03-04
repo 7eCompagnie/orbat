a367e62905b7372fce2d30d503e39c377f72a5ef
Clément <clement.ozor@protonmail.com>
2023-03-05 00:19:33 +0100

    [main] => Install and configure Prettier for ESLint

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home (): JSX.Element {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
